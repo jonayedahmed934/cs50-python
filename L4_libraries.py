69084a880038bc0b5f533c8faea3ec3d638f7dce
jonayedahmed934 <jonayedahmed934@gmail.com>
2025-05-05 05:20:08 +0900

    this the file i/o section

--- L4_libraries.py
+++ L4_libraries.py
@@ -1,6 +1,11 @@
-import sys
-import requests
+i = 3
 
-resposnse = requests.get("https://www.youtube.com/watch?v=MztLZWibctI&list=PLhQjrBD2T3817j24-GogXmWqO5Q5vYy0V&index="+sys.argv[1])
+while i!=0:
+    with open("savefile.txt","a") as file:
+        file.write(input("say what you wanna say: ")+"\n")
+        
+    i-=1
+with open("savefile.txt", "r") as file:
+    lines = file.readlines()
 
-print(resposnse.json())
+print(lines)
